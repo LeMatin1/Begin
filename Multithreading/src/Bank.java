4811d5cca3159090e4a90f8839888d85b545214e
LeMatin1 <q1186538850@qq.com>
2019-06-19 15:01:32 +0800

    one

--- Multithreading/src/Bank.java
+++ Multithreading/src/Bank.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Author: LeMatin (Administrator)
  * @Date: 2019/6/19 13:51
@@ -7,7 +9,15 @@ public class Bank {
 
     public static void main(String[] args) {
 
+        Person person = new Person();
 
+        Thread t1 = new Thread(person);
+        t1.setName("1");
+        Thread t2 = new Thread(person);
+        t2.setName("2");
+
+        t1.start();
+        t2.start();
 
     }
 
@@ -15,9 +25,52 @@ public class Bank {
 
 class Person implements Runnable{
 
+    private int add = 1000;
+    private int Count = 3000;
+    private ReentrantLock lock = new ReentrantLock(true);
+
+    public Person() {
+
+    }
 
     @Override
-    public void run() {
+//    public synchronized void  run() {
+//
+//        for (int i = 0; i < 3; i++) {
+//            Count+=add;
+//            System.out.println(Thread.currentThread().getName()+"存"+Count);
+//        }
+//
+//    }
+
+//    public void run(){
+//
+//        synchronized (this) {
+//            for (int i = 0; i < 3; i++) {
+//                Count+=add;
+//                System.out.println(Thread.currentThread().getName()+"存"+Count);
+//            }
+//        }
+//
+//    }
+
+
+    public void run(){
+
+        try{
+            lock.lock();
+
+            for (int i = 0; i < 3; i++) {
+                Count+=add;
+                System.out.println(Thread.currentThread().getName()+"存"+Count);
+            }
+
+        }finally {
+
+            lock.unlock();
+
+        }
+
 
     }
 }
